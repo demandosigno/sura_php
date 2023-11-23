069444b8620faef11fba0bb146ee0f14d1442366
demandosigno <34424426+demandosigno@users.noreply.github.com>
2023-11-23 19:27:41 +0900

    9章の2まで進んだ
    ファイルの読み込み方法

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+//$info = file_get_contents("./progress/progress.txt");
+$fp = fopen("./progress/progress.txt", "r"); //ファイルを開く
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPの学習</title>
+</head>
+
+<body>
+    <h1>PHPの学習</h1>
+    <h2>進捗状況</h2>
+    <?php
+    //var_dump($fp);
+    //ファイルが正しく開けたとき
+    if ($fp) {
+        $title = fgets($fp); //ファイルから1行読み込む
+        if ($title) {
+            echo '<a href="progress.php">' . $title . '</a>';
+        } else {
+            //ファイルの中身が空だったとき
+            echo '進捗ゼロです。';
+        }
+        fclose($fp); //ファイルを閉じる
+    } else {
+        //ファイルが開けなかったとき
+        echo '進捗ゼロです。';
+    }
+    ?>
+</body>
+
+</html>
---
+++ progress.php
@@ -0,0 +1,51 @@
+<?php
+$fp = fopen("./progress/progress.txt", "r"); //ファイルを開く
+$line = array(); //ファイル内容を1行1要素に格納する配列
+//$line2 = file("./progress/progress.txt");
+//var_dump($line2);
+//ファイルが正しく開けたとき
+if ($fp) {
+    while (!feof($fp)) {
+        $line[] = fgets($fp);
+    }
+    fclose($fp);
+}
+//var_dump($line);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>進捗状況</title>
+</head>
+
+<body>
+    <h1>PHPの学習</h1>
+    <h2>進捗状況</h2>
+    <?php
+    //var_dump(count($line));
+    if (count($line) > 0) {
+        /* for ($i = 0; $i < count($line); $i++) {
+            if ($i == 0) { //はじめの行はタイトル
+                echo '<h3>' . $line[0] . '</h3>';
+            } else {
+                echo $line[$i] . '<br>';
+            }
+        }*/
+        foreach ($line as $i => $text) {
+            if ($i == 0) {
+                echo '<h3>' . $text . '</h3>';
+            } else {
+                echo $text . '<br>';
+            }
+        }
+    } else {
+        //ファイルの中身が空だったとき
+        echo '進捗ゼロです。';
+    }
+    ?>
+</body>
+
+</html>
